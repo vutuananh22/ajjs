136b2e75537154f21d594244335366a946a59f77
vutuananh22 <110403011+vutuananh22@users.noreply.github.com>
2022-10-05 21:27:49 +0700

    upcode

--- src/app/thoitiet/thoitiet.component.ts
+++ src/app/thoitiet/thoitiet.component.ts
@@ -13,7 +13,11 @@ export class ThoitietComponent{
   }
   ngOnInit(){
     this.hanoi();
-
+  this.sg();
+  this.ld();
+  this.ny();
+  this.tko();
+  this.bl();
   }
   hanoi(){
     const url='https://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid=09a71427c59d38d6a34f89b47d75975c&units=metric'
@@ -25,5 +29,54 @@ export class ThoitietComponent{
         this.city = data.name +","+ data.sys.country;
       })
 }
-
+  sg(){
+    const url='https://api.openweathermap.org/data/2.5/weather?q=saigon&appid=09a71427c59d38d6a34f89b47d75975c&units=metric'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.temp = data.main.temp;
+        this.hump = data.main.humidity;
+        this.press = data.main.pressure;
+        this.city = data.name +","+ data.sys.country;
+      })
+  }
+  ld(){
+    const url='https://api.openweathermap.org/data/2.5/weather?q=london&appid=09a71427c59d38d6a34f89b47d75975c&units=metric'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.temp = data.main.temp;
+        this.hump = data.main.humidity;
+        this.press = data.main.pressure;
+        this.city = data.name +","+ data.sys.country;
+      })
+  }
+  ny(){
+    const url='https://api.openweathermap.org/data/2.5/weather?q=new york&appid=09a71427c59d38d6a34f89b47d75975c&units=metric'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.temp = data.main.temp;
+        this.hump = data.main.humidity;
+        this.press = data.main.pressure;
+        this.city = data.name +","+ data.sys.country;
+      })
+  }
+  tko(){
+    const url='https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=09a71427c59d38d6a34f89b47d75975c&units=metric'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.temp = data.main.temp;
+        this.hump = data.main.humidity;
+        this.press = data.main.pressure;
+        this.city = data.name +","+ data.sys.country;
+      })
+  }
+  bl(){
+    const url='https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=09a71427c59d38d6a34f89b47d75975c&units=metric'
+    this.http.get<any>(url)
+      .subscribe(data=>{
+        this.temp = data.main.temp;
+        this.hump = data.main.humidity;
+        this.press = data.main.pressure;
+        this.city = data.name +","+ data.sys.country;
+      })
+  }
 }
